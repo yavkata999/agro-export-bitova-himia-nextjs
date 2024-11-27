b1e45354fe36dfb27f85159253665bb875dfd89c
yavkata999 <yavor.kr@gmail.com>
2024-11-27 11:13:29 +0200

    even more optimizations

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,41 +1,20 @@
-import dynamic from "next/dynamic";
 import parse from "html-react-parser";
+import { Card, AspectRatio, Text, Spoiler } from "@mantine/core";
+import Image from "next/image";
 import styles from "../styles/ProductCard.module.css";
-
-const Card = dynamic(() => import("@mantine/core").then((mod) => mod.Card), {
-  ssr: false,
-});
-const AspectRatio = dynamic(() =>
-  import("@mantine/core").then((mod) => mod.AspectRatio)
-, {
-  ssr: false,
-});
-const Text = dynamic(() => import("@mantine/core").then((mod) => mod.Text), {
-  ssr: false,
-});
-const Spoiler = dynamic(() =>
-  import("@mantine/core").then((mod) => mod.Spoiler)
-, {
-  ssr: false,
-});
-const Image = dynamic(() => import("next/image"));
 
 export function ProductCard({ image, title, description }) {
   return (
-    <Card key={title} p="md" radius="md" shadow="xl" withBorder>
+    <Card p="md" radius="md" shadow="xl" withBorder>
       <AspectRatio ratio={8 / 9}>
         <Image
           src={image}
           alt="Produktova snimka"
-          width={0}
-          height={0}
-          sizes="(max-width: 768px) 100vw, 33vw"
+          width={300} // Define a reasonable width
+          height={337} // Match the aspect ratio (8:9)
           style={{
-            width: "100%",
-            height: "100%",
             objectFit: "contain",
           }}
-          priority
         />
       </AspectRatio>
       <Text className={styles["cardTitle"]} mt={5} align="center">
